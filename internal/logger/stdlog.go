8b718895b8e236b2de2661831861b135414c9c08
yumosx <zhengel2022@163.com>
2025-07-19 23:47:43 +0800

    feat(logger): add colored log output for different log levels in terminal

--- internal/logger/stdlog.go
+++ internal/logger/stdlog.go
@@ -19,18 +19,44 @@ import (
 	"log"
 )
 
+const (
+	colorReset  = "\033[0m"
+	colorGray   = "\033[37m"
+	colorBlue   = "\033[34m"
+	colorGreen  = "\033[32m"
+	colorYellow = "\033[33m"
+	colorRed    = "\033[31m"
+	colorWhite  = "\033[97m"
+	colorBgRed  = "\033[41m"
+)
+
 type stdImplementation struct {
 	logger *log.Logger
 }
 
 func newStdImplementation(out io.Writer) *stdImplementation {
 	return &stdImplementation{
-		logger: log.New(out, "", log.Ldate|log.Ltime|log.Lmicroseconds|log.LUTC),
+		logger: log.New(out, "", log.Ldate|log.Ltime|log.Lmicroseconds|log.Lmicroseconds),
 	}
 }
 
 func (s *stdImplementation) format(prefix string, msg string) string {
-	return prefix + msg
+	var color string
+	switch prefix {
+	case "DEBUG: ":
+		color = colorGray
+	case "INFO: ":
+		color = colorGreen
+	case "WARN: ":
+		color = colorYellow
+	case "ERROR: ":
+		color = colorRed
+	case "FATAL: ":
+		color = colorBgRed + colorWhite
+	default:
+		color = colorReset
+	}
+	return color + prefix + msg + colorReset
 }
 
 func (s *stdImplementation) Debug(msg string) {
